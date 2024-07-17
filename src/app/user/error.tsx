2204c19c894a60a1557b810e2bb0f36774232ed5
Emmanuel <onyejemeemmanuel65@gmail.com>
2024-07-17 08:17:39 +0100

    Set up user page

---
+++ src/app/user/error.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserErrorPage = () => {
+    return (
+        <div>error</div>
+    )
+}
+
+export default UserErrorPage
---
+++ src/app/user/page.tsx
@@ -0,0 +1,7 @@
+const UserHomePage = async () => {
+    return (
+        <div>page</div>
+    )
+}
+
+export default UserHomePage;
